5a08e8ca0f8cde6bfa41d4b11e16d660cd1b731b
tmacal <tadmcallister@aim.com>
2017-12-18 01:22:33 +0000

    rearranging main

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -1,62 +1,51 @@
 import os
 import sys
 import time
-import pyaudio
 import numpy as np
 import speech_recognition as sr
 from google.cloud import language
 from google.cloud.language import enums
 from google.cloud.language import types
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="C:\\Users\\Tad\\Desktop\\TargetedAdvertising-dc956f1a9b6b.json"
-from colorama import init, Fore, Back, Style
 from PIL import Image
+# from colorama import init, Fore, Back, Style ## include - init()
+# from pocketsphinx import pocketsphinx
 
 def makeSentimentImg(sentiment, text):
-    init()
     if -1.0 <= sentiment.score <= -0.25:
-        print (Back.RED + ("\nBad: {}".format(text)))
+        print("\nBad: {}".format(text))
         # array = np.zeros([720, 1080, 3], dtype=np.uint8)
         # array[:, :] = [255, 0, 0]  # 4 = alpha channel
         # imgbad = Image.fromarray(array)
         # imgbad.save('testr.png')
         imgbad = Image.open('testr.png')
         imgbad.show()
-        imgbad.close()
 
     elif -0.25 <= sentiment.score <= 0.25:
-        print (Back.CYAN + ("\nNeutral: {}".format(text)))
+        print("\nNeutral: {}".format(text))
         # array = np.zeros([720, 1080, 3], dtype=np.uint8)
         # array[:, :] = [0, 0, 255]  # 4 = alpha channel
         # imgneut = Image.fromarray(array)
         # imgneut.save('testb.png')
         imgneut = Image.open('testb.png')
         imgneut.show()
-        imgneut.close()
 
     elif 0.25 <= sentiment.score <= 1.0:
-        print (Back.GREEN + ("\nGood: {}".format(text)))
+        print("\nGood: {}".format(text))
         # array = np.zeros([720, 1080, 3], dtype=np.uint8)
         # array[:, :] = [0, 128, 0]  # 4 = alpha channel
         # imggood = Image.fromarray(array)
         # imggood.save('testg.png')
         imggood = Image.open('testg.png')
         imggood.show()
-        imggood.close()
 
 def printOver(str):
     sys.stdout.write("\r" + (str))
     sys.stdout.flush()
 
-def progressBar(text):
-    for i in range(len(text)):
-        time.sleep(1)
-        sys.stdout.write("\r%d%%" % (i/len(text)*100))
-        sys.stdout.flush()
-
 def makeImg(sentimentScr):
     array = np.zeros([720, 1080, 3], dtype=np.uint8)
-    'R                           G                         B'
-    array[:, :] = [sentimentScr, sentimentScr, sentimentScr]
+    array[:, :] = [sentimentScr, sentimentScr, sentimentScr] #RGB values from range 0 - 255
     madeImg = Image.fromarray(array)
     madeImg.save('madeImg.png')
     madeImg = Image.open('madeImg.png')
@@ -68,42 +57,52 @@ def record():
     try:
         with sr.Microphone(device_index=1) as source:
             # print('recording...')
-            printOver('recording...')
+            printOver('listening...')
             # r.adjust_for_ambient_noise(source)
             # r.energy_threshold ##edit*
             audio = r.listen(source)
-            r.recognize_google(audio)
-            # print('understood...')
+
+        try:
+            text = r.recognize_google(audio)
             printOver('understood...')
 
-            # Instantiates a client
-            client = language.LanguageServiceClient()
+            try:
+                document = types.Document(
+                    content=text,
+                    type=enums.Document.Type.PLAIN_TEXT)
+                # instantiates client
+                client = language.LanguageServiceClient()
+                # Detects sentiment
+                printOver('interpreting...')
+                sentiment = client.analyze_sentiment(document=document).document_sentiment
+                sentimentScr = int(round((sentiment.score + 1) *(255/2)))
+                sentimentMag = int(round((sentiment.score + 1) *(255/2)))
 
-            # The text to analyze
-            text = r.recognize_google(audio)
-            # progressBar(text) #percentage progress bar
-            # print('interpreting...')
-            printOver('interpreting...')
-            document = types.Document(
-                content=text,
-                type=enums.Document.Type.PLAIN_TEXT)
+                # print('feeling...')
+                printOver('feeling...')
+                makeSentimentImg(sentiment, text) ##generates and displays image
+                print('\nSentiment: {}, {}'.format(sentiment.score, sentiment.magnitude))
+                print('SentimentScr: ' + str(sentimentScr))
+                print('SentimentMag: ' + str(sentimentMag))
 
-            # Detects sentiment
-            sentiment = client.analyze_sentiment(document=document).document_sentiment
-            sentimentScr = int(round((sentiment.score + 1) *(255/2)))
-            sentimentMag = int(round((sentiment.score + 1) *(255/2)))
+            except Exception as e:
+                print('\nFailed to process sentiment' + str(e))
 
-            # print('feeling...')
-            printOver('feeling...')
-            makeSentimentImg(sentiment, text) ##displays image
-            print ('\nSentiment: {}, {}'.format(sentiment.score, sentiment.magnitude))
-            print('SentimentScr: ' + str(sentimentScr))
-            print('SentimentMag: ' + str(sentimentMag))
+        except sr.UnknownValueError:
+            print("Google Speech Recognition could not understand audio")
+        except sr.RequestError as e:
+            print("Could not request results from Google Speech Recognition service; {0}".format(e))
 
-    except sr.UnknownValueError:
-        print("Google Speech Recognition could not understand audio")
-    except sr.RequestError as e:
-        print("Could not request results from Google Speech Recognition service; {0}".format(e))
+        # try:
+        #     text = r.recognize_sphinx(audio)
+        #     printOver('understood...')
+        # except sr.UnknownValueError:
+        #     print("Sphinx could not understand audio")
+        # except sr.RequestError as e:
+        #     print("Sphinx error; {0}".format(e))
+
+    except Exception as e:
+        print('\nSomething went wrong' + str(e)) #general error handler
 
 # #check input devices
 # p = pyaudio.PyAudio()
@@ -112,4 +111,3 @@ def record():
 #     print(info['index'], info['name'])
 while True:
     record()
-    # time.sleep(10)
