3fab02af1c7c7fe5924f3665b731c2894f6a8f77
tmacal <tadmcallister@aim.com>
2018-01-27 18:20:09 +0000

    host querying

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -1,106 +1,96 @@
 import os
 import sys
+import socket
 import pyaudio
+import colorsys
 import numpy as np
 import speech_recognition as sr
 import subprocess as sp
 import pigpio
-from googleads import adwords
 from google.cloud import language
 from google.cloud.language import enums
 from google.cloud.language import types
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"]="C:\\Users\\Tad\\Desktop\\TargetedAdvertising-dc956f1a9b6b.json"
 from PIL import Image
 
-##set up pigpio daemon on raspberryPi
-# @reboot         /usr/local/bin/pigpiod
-# Use
-#
-# sudo crontab -e
-#
-# to edit the root crontab and add that line to the end. Then ctrl-o return ctrl-x to exit.
-ip = "192.168.1.10"
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "C:\\Users\\Tad\\Desktop\\TargetedAdvertising-dc956f1a9b6b.json"
 
-pi = pigpio.pi(ip, 8888) #HOST, PORT
+ip = "192.168.1.10"
+pi = pigpio.pi(ip, 8888)  # HOST, PORT
 print(pi.connected)
 RED_PIN = 10
 GREEN_PIN = 9
 BLUE_PIN = 11
+
 
 def setRedInten(redVal):
     pi.set_PWM_dutycycle(RED_PIN, redVal)
     pi.write(10, 1)
     pi.write(9, 0)
     pi.write(11, 0)
+
+
 def setGreenInten(greenVal):
     pi.write(9, 1)
     pi.write(10, 0)
     pi.write(11, 0)
+
+
 def setBlueInten(blueVal):
     pi.set_PWM_dutycycle(BLUE_PIN, blueVal)
     pi.write(11, 1)
     pi.write(9, 0)
     pi.write(10, 0)
 
+
 def ipcheck():
-    status, result = sp.getstatusoutput('ping ' + ip)
-    if status == 0:
-        print('tadPi@ ' + ip + ' is UP')
-        print(result)
-    else:
-        print('tadPi@ ' + ip + ' is DOWN')
+    try:
+        status, result = sp.getstatusoutput('ping ' + ip)
+        hostname = socket.gethostbyaddr(ip)
+        print (hostname)
+        if status == 0 and "pi" in hostname:
+            print('tadPi@ ' + ip + ' is UP')
+            print(result)
+        else:
+            print('tadPi@ ' + ip + ' is DOWN')
+    except Exception as e:
+        print("\nCouldn't contact host " + str(e))
+
 
 def makeSentimentImg(sentiment, text):
     if -1.0 <= sentiment.score <= -0.25:
         print("\nBad: {}".format(text))
         setRedInten(255)
-        # array = np.zeros([720, 1080, 3], dtype=np.uint8)
-        # array[:, :] = [255, 0, 0]  # 4 = alpha channel
-        # imgbad = Image.fromarray(array)
-        # imgbad.save('testr.png')
-        # imgbad = Image.open('testr.png')
-        # imgbad.show()
-
     elif -0.25 <= sentiment.score <= 0.25:
         print("\nNeutral: {}".format(text))
         setBlueInten(255)
-        # array = np.zeros([720, 1080, 3], dtype=np.uint8)
-        # array[:, :] = [0, 0, 255]  # 4 = alpha channel
-        # imgneut = Image.fromarray(array)
-        # imgneut.save('testb.png')
-        # imgneut = Image.open('testb.png')
-        # imgneut.show()
-
     elif 0.25 <= sentiment.score <= 1.0:
         print("\nGood: {}".format(text))
         setGreenInten(255)
-        # array = np.zeros([720, 1080, 3], dtype=np.uint8)
-        # array[:, :] = [0, 128, 0]  # 4 = alpha channel
-        # imggood = Image.fromarray(array)
-        # imggood.save('testg.png')
-        imggood = Image.open('testg.png')
-        # imggood.show()
+
+    # hsvColor = (sentiment.score, sentiment.magnitude, 1)
+    # rgbConversion = colorsys.hsv_to_rgb((round(sentiment.score),1), (round(sentiment.magnitude),1), 1)
+    # print (rgbConversion)
+
+    array = np.zeros([720, 1080, 3], dtype=np.uint8)
+    array[:, :] = [0, 0, 255]  # 4 = alpha channel
+    imgnew = Image.fromarray(array)
+    imgnew.save('color.png')
+    imgnew = Image.open('color.png')
+    imgnew.show()
+
 
 def printOver(str):
     sys.stdout.write("\r" + (str))
     sys.stdout.flush()
 
-def makeImg(sentimentScr):
-    array = np.zeros([720, 1080, 3], dtype=np.uint8)
-    array[:, :] = [sentimentScr, sentimentScr, sentimentScr] #RGB values from range 0 - 255
-    madeImg = Image.fromarray(array)
-    madeImg.save('madeImg.png')
-    madeImg = Image.open('madeImg.png')
-    madeImg.show()
-    madeImg.close()
 
 def record():
     r = sr.Recognizer()
     try:
-        with sr.Microphone(device_index=1, sample_rate = 48000) as source:
+        with sr.Microphone(device_index=1, sample_rate=48000) as source:
             printOver('listening...')
             # r.adjust_for_ambient_noise(source)
-            # r.energy_threshold ##edit*
+            # r.energy_threshold #edit
             audio = r.listen(source)
 
         try:
@@ -111,19 +101,20 @@ def record():
                 document = types.Document(
                     content=text,
                     type=enums.Document.Type.PLAIN_TEXT)
-                # instantiates client
                 client = language.LanguageServiceClient()
-                # Detects sentiment
                 printOver('interpreting...')
                 sentiment = client.analyze_sentiment(document=document).document_sentiment
-                sentimentScr = int(round((sentiment.score + 1) *(255/2)))
-                sentimentMag = int(round((sentiment.score + 1) *(255/2)))
+                sentimentScr = int(round((sentiment.score + 1) * (255 / 2)))
+                sentimentMag = int(round((sentiment.magnitude + 1) * (255 / 2)))
                 printOver('feeling...')
-                makeSentimentImg(sentiment, text) ##generates and displays image
-                # makeColor(sentimentScr)
                 print('\nSentiment: {}, {}'.format(sentiment.score, sentiment.magnitude))
                 print('SentimentScr: ' + str(sentimentScr))
                 print('SentimentMag: ' + str(sentimentMag))
+
+                try:
+                    makeSentimentImg(sentiment, text)  ##generates and displays image
+                except Exception as e:
+                    print("\nCouldn't create image " + str(e))
 
             except Exception as e:
                 print('\nFailed to process sentiment ' + str(e))
@@ -134,9 +125,10 @@ def record():
             print("Could not request results from Google Speech Recognition service; {0}".format(e))
 
     except Exception as e:
-        print('\nSomething went wrong ' + str(e)) #general error handler
+        print('\nSomething went wrong ' + str(e))
 
-#system and input devices check
+
+# system and input devices check
 ipcheck()
 p = pyaudio.PyAudio()
 for i in range(p.get_device_count()):
